54dc08b82a8ab0ba8ef1e36e3bba9a108e1af4ae
rashid_aliyev96@outlook.com <rashid_aliyev96@outlook.com>
2024-05-02 18:19:42 +0400

    Add project files.

---
+++ FreelanceTestApp/EmailSending.cs
@@ -0,0 +1,34 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreelanceTestApp
+{
+    public class EmailSending
+    {
+        public async Task Send(string senderName, string senderEmail, string recipientName, string recipientEmail, string text)
+        {           
+            var client = new RestClient("https://send.api.mailtrap.io/api");
+
+            var args_ = new
+            {
+                from = new { email = senderEmail, name = senderName },
+                to = new[] { new { email = recipientEmail, name = recipientName } },
+                subject = "Awesome",
+                text = text,
+            };
+
+            var request = new RestRequest("/send", RestSharp.Method.Post);
+
+            request.AddHeader("Api-Token", "6c3c24289024661017339035acafb343");
+            request.AddJsonBody(args_);
+
+            RestResponse response = await client.ExecuteAsync(request);
+
+            Console.WriteLine(response.Content);
+        }
+    }
+}
---
+++ FreelanceTestApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using FreelanceTestApp;
+
+
+var emailSending = new EmailSending();
+
+await emailSending.Send("Mailtrap", "mailtrap@demomailtrap.com", 
+                        "Receiver", "rashid.aliyev96@gmail.com", 
+                        "Hi there ! Hello to my freelance test app !");
